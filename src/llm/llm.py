7ead4ca95bd6f2d60c53ddf51cf3c2d1327dd483
Haroon Akram <haroonakram080@gmail.com>
2024-07-23 19:59:11 -0400

    updating code to be simpler

--- src/llm/llm.py
+++ src/llm/llm.py
@@ -1,16 +1,20 @@
+import ollama
+
+from src.globals import DEFAULT_SYSTEM_PROMPT
+
 class LLM:
     def __init__(self, model_name: str):
         # Import statements are done in these if blocks so that we don't overwrite the setting of environment variables
-        if model_name == "phi3_mini":
-            from src.llm.phi3_mini import Phi3_Mini
-            self.model = Phi3_Mini()
-
-        elif model_name == "llama3":
-            from src.llm.llama3 import Llama3
-            self.model = Llama3()
-
-        else:
-            raise ValueError("Parameter 'model_name' must be 'phi3_mini' or 'llama3'")
+        if model_name not in ["phi3:mini", "llama3"]:
+            raise ValueError("Parameter 'model_name' must be 'phi3:mini' or 'llama3'")
+        
+        self.model_name = model_name
     
     def chat(self, prompt: str):
-        return self.model.chat(prompt=prompt)
+        response = ollama.chat(
+            model=self.model_name,
+            messages=[DEFAULT_SYSTEM_PROMPT[self.model_name], {'role': 'user', 'content': prompt}],
+            stream=False,
+        )
+
+        return response['message']['content']
--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from src.drone_manager import DroneManager
 
 def main():
-    drone = DroneManager(model_name="phi3_mini")
+    drone = DroneManager(model_name="llama3")    # Use "phi3:mini" or "llama3" tto switch between models
     drone.listen()
 
 if __name__ == "__main__":
