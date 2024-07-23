e0ee08ece4370b185e2b51f0e08eaa8efe7b0e64
Haroon Akram <haroonakram080@gmail.com>
2024-07-23 19:33:50 -0400

    Implementing system prompt into the model

--- src/globals.py
+++ src/globals.py
@@ -5,3 +5,7 @@ LLAMA_3_PATH = "./models/base_llama_3"
 # HuggingFace Model Checkpoints
 PHI_3_MINI_CHECKPOINT = "microsoft/Phi-3-mini-4k-instruct"
 LLAMA_3_CHECKPOINT = "meta-llama/Meta-Llama-3-8B"
+
+# System Prompts
+LLAMA3_DEFAULT_SYSTEM_PROMPT = {"role": "system", "content": "You convert sentences into instructions for a drone. These are the possible drone instructions: UP, DOWN, LEFT, RIGHT, FORWARD, BACKWARD, ROTATE X (where X is the direction). Each instruction must be separated by a comma."}
+PHI3_MINI_DEFAULT_SYSTEM_PROMPT = {"role": "system", "content": "You convert sentences into instructions for a drone. These are the possible drone instructions: UP, DOWN, LEFT, RIGHT, FORWARD, BACKWARD, ROTATE X (where X is the direction). Each instruction must be separated by a comma."}
