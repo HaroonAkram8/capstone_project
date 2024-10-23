8f347c9fc570fec8014c38e7f84553b729edf40f
Haroon Akram <haroonakram080@gmail.com>
2024-10-23 18:10:56 -0400

    implementing map

--- src/api/api.py
+++ src/api/api.py
@@ -1,6 +1,10 @@
 import airsim
 import time
 
+from src.globals import (
+    MOVE_POS, MOVE_VEL, ROTATE, TAKEOFF, LAND, END
+)
+
 class DroneController():
     def __init__(self):
         self.client = airsim.MultirotorClient()
@@ -8,38 +12,35 @@ class DroneController():
         self.client.enableApiControl(True)
         self.client.armDisarm(True)
 
-        print("Drone initialized and ready.")
+        self.functions = {
+            MOVE_POS: self.client.moveToPositionAsync,
+            MOVE_VEL: self.client.moveByVelocityAsync, # May need to be removed
+            ROTATE: self.client.rotateToYawAsync,
+            TAKEOFF: self.client.takeoffAsync,
+            LAND: self.client.landAsync,
+            END: self.__end__,
+        }
     
-    def takeoff(self, altitude=5):
-        print("Taking off...")
-
-        self.client.takeoffAsync().join()
-        self.client.moveToZAsync(-altitude, 5).join()
-
-        print(f"Reached altitude: {altitude} meters")
-
-    def land(self):
-        print("Landing...")
-
-        self.client.landAsync().join()
+    def __end__(self):
         self.client.armDisarm(False)
         self.client.enableApiControl(False)
-
-        print("Drone landed and disarmed.")
 
 # Example usage
 if __name__ == "__main__":
     drone = DroneController()
 
-    velocity = {"x": 5, "y": 10, "z": 7}
+    position = {"x": 5, "y": 10, "z": -7}
     rotate = {"angle_deg": 90, "rate_deg_per_sec": 30}
 
     # Perform basic movements
-    drone.takeoff()
-    drone.client.moveByVelocityAsync(velocity["x"], velocity["y"], velocity["z"], duration=5).join()
+    drone.functions[TAKEOFF]().join()
+    # print(drone.client.getMultirotorState())
+    drone.functions[ROTATE](rotate["angle_deg"]).join()
     print("movement 1 done")
-    drone.client.moveByVelocityAsync(-velocity["x"], -velocity["y"], -velocity["z"], duration=5).join()
+    drone.functions[MOVE_POS](position["x"], position["y"], position["z"], velocity=5).join()
     print("movement 2 done")
-    drone.client.goHomeAsync().join()
-    print("Returned home")
-    drone.land()
+    drone.functions[MOVE_POS](0, 0, -1, velocity=5).join()
+    time.sleep(3)
+    drone.functions[LAND]().join()
+
+    drone.functions[END]()
