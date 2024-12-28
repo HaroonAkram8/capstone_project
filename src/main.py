3cf5438b7e83e5cbe7513fa0c4bbff3b48913d13
TZProgrammer <thomaszeger@gmail.com>
2024-12-28 12:01:14 -0500

    rename system prompt to indicate it is the default

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from src.llm.llm import Models
 def main(model: Models):
     "Takes a model and runs it with our system prompt"
     system_prompt = ""
-    with open("./system_prompts/llama3_1.txt", "r") as file:
+    with open("./system_prompts/default_system_prompt.txt", "r") as file:
         system_prompt = file.read().lower()
 
     drone = DroneManager(model=model, system_prompt=system_prompt)
