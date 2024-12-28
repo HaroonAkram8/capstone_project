51ae4acbca923bece9eacf32043b52a858fe84de
TZProgrammer <thomaszeger@gmail.com>
2024-12-28 12:04:50 -0500

    add function to run llama3_1

--- src/main.py
+++ src/main.py
@@ -10,6 +10,10 @@ def main(model: Models):
     drone = DroneManager(model=model, system_prompt=system_prompt)
     drone.listen()
 
+def run_llama3_1():
+    "Runs the llama3_1 model with our system prompt"
+    main(Models.LLAMA3_1)
+
 def run_phi3_mini():
     "Runs the phi3_mini model with our system prompt"
     main(Models.PHI3_MINI)
