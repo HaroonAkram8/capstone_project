fd504675a3caff74d1e8529f2414acd0a850153b
TZProgrammer <thomaszeger@gmail.com>
2024-12-28 11:58:52 -0500

    add system prompt to models being run

--- src/drone_manager.py
+++ src/drone_manager.py
@@ -2,8 +2,8 @@ from src.llm.llm import LLM, Models
 from src.speech_to_text.speech_to_text import speech_to_text
 
 class DroneManager:
-    def __init__(self, model: Models) -> None:
-        self.model = LLM(model=model)
+    def __init__(self, model: Models, system_prompt: str) -> None:
+        self.model = LLM(model=model, system_prompt=system_prompt)
     
     def listen(self) -> None:
         while(True):
--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from src.drone_manager import DroneManager
 from src.llm.llm import Models
 
-def main():
-    drone = DroneManager(model=Models.PHI3_MINI)
+def main(model: Models):
+    "Takes a model and runs it with our system prompt"
+    system_prompt = ""
+    with open("./system_prompts/llama3_1.txt", "r") as file:
+        system_prompt = file.read().lower()
+
+    drone = DroneManager(model=model, system_prompt=system_prompt)
     drone.listen()
 
+def run_phi3_mini():
+    "Runs the phi3_mini model with our system prompt"
+    main(Models.PHI3_MINI)
+
 if __name__ == "__main__":
-    main()
+    run_phi3_mini()
