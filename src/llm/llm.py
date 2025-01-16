faefac03280b463eb19d0d9a6356ab6003e71461
Haroon Akram <haroonakram080@gmail.com>
2025-01-15 20:37:55 -0500

    implement point cloud

--- src/llm/llm.py
+++ src/llm/llm.py
@@ -1,15 +1,12 @@
 import ollama
 from enum import Enum
 
-
-
 class Models(Enum):
     LLAMA3_2 = "llama3.2"
     LLAMA3_1 = "llama3.1"
     LLAMA3_1_70 = "llama3.1:70b"
     LLAMA3 = "llama3"
     PHI3_MINI = "phi3:mini"
-
 
 class LLM:
     def __init__(self, model: Models, system_prompt: str):
